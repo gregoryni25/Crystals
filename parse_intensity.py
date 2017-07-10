745420d4584d900a4caadb81f2cbdc454e6c3ece
gregoryni25 <ghn@andrew.cmu.edu>
2017-07-10 13:39:40 -0700

    optimized for only necessary pathlengths

--- parse_intensity.py
+++ parse_intensity.py
@@ -8,8 +8,14 @@ def pathlength(D, fcoor, parent):
     pathlength from root ray (ray 0) to that ray
     """
     coor = D[parent][1]
-    return D[parent][2] + np.linalg.norm(np.subtract(fcoor, coor))
-
+    
+    if D[parent][0] == "base":
+        return np.linalg.norm(np.subtract(fcoor, coor))
+    
+    if D[parent][0] >= 0:
+        D[parent][0] = -pathlength(D, coor, D[parent][0])
+        
+    return np.linalg.norm(np.subtract(fcoor, coor)) - D[parent][0]    
 
 def parse_segs_data(D, aline):
     """
@@ -21,11 +27,8 @@ def parse_segs_data(D, aline):
     if int(aline[4]) == 3:
         return [-3, int(aline[1]), (float(aline[9]), float(aline[10]),
                  float(aline[11])), float(aline[12])]
-<<<<<<< HEAD
+
     # hit face 4, the back detector
-=======
-    # hit face 4, the front detector
->>>>>>> c641a7aebb23d38f83ed47ddcfd85241447bf517
     if int(aline[4]) == 4:
         return [-4, int(aline[1]), (float(aline[9]), float(aline[10]),
                  float(aline[11])), float(aline[12])]
@@ -33,19 +36,14 @@ def parse_segs_data(D, aline):
     # if it didn't hit detector 3 or 4 but still terminated, then don't store
     if aline[6] == '*---':
         return [0]
-<<<<<<< HEAD
     
     # base case
     if aline[0] == '0':
-        return [int(aline[1]), (float(aline[9]), float(aline[10]),
+        return ["base", (float(aline[9]), float(aline[10]),
              float(aline[11])), 0]
-        
-=======
->>>>>>> c641a7aebb23d38f83ed47ddcfd85241447bf517
 
     return [int(aline[1]), (float(aline[9]), float(aline[10]),
-             float(aline[11])), pathlength(D, (float(aline[9]), 
-                   float(aline[10]), float(aline[11])), int(aline[1]))]
+             float(aline[11]))]
 
 def parse_segs(fid, numseg):
     """
@@ -60,11 +58,9 @@ def parse_segs(fid, numseg):
 
     for idx in range(numseg+1):
         aline = fid.readline()
-<<<<<<< HEAD
+
         D.append(parse_segs_data(D, aline))
-=======
-        D.append(parse_segs_data(aline))
->>>>>>> c641a7aebb23d38f83ed47ddcfd85241447bf517
+
         if D[idx][0] == -3:
             R.append((1, D[idx][3], pathlength(D, D[idx][2], D[idx][1])))
         if D[idx][0] == -4:
@@ -120,13 +116,9 @@ def parse(filename, numrays):
 t = time.time()
 
 A = np.array(parse('5000-4.txt',5000))
-print(time.time()-t)
-t = time.time()
 B = np.array(parse('5000-3.txt',5000))
-print(time.time()-t)
-t = time.time()
 C = np.array(parse('5000-2.txt',5000))
-print(time.time()-t)
+
 pl.hist(A[:,2], bins=20, weights=A[:,1], alpha=0.5, label="four")
 pl.hist(B[:,2], bins=20, weights=B[:,1], alpha=0.5, label="three")
 pl.hist(C[:,2], bins=20, weights=C[:,1], alpha=0.5, label="two")
