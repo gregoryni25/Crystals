ef092c6b5fc9652f5bdb40004d631c46a312a65c
gregoryni25 <ghn@andrew.cmu.edu>
2017-08-07 10:59:14 -0700

    Added level

--- parse_intensity.py
+++ parse_intensity.py
@@ -17,7 +17,7 @@ def trape_dens(edge1, edge2, z, n, c, nn):
     y = dens_m(z, n, c, t)
     return np.trapz(y, t)
 
-def theoretical_bins(edges, z=None, n=1.82, c=299792458, nn=100, NN=1):
+def theoretical_bins(edges, z=None, n=1.82, c=299792458000, nn=100, NN=10000):
     '''
     edges       bin edges in time (s)
     z           distance from source to end cap
@@ -53,7 +53,7 @@ def pathlength(D, fcoor, parent):
     
     return np.linalg.norm(np.subtract(fcoor, coor)) - D[parent][0]    
 
-def parse_segs_data(D, aline):
+def parse_segs_data(D, aline, level=False):
     """
     a utility used by parse_segs. gathers relevant line data
     """
@@ -61,11 +61,17 @@ def parse_segs_data(D, aline):
 
     # hit face 3, the front detector
     if int(aline[4]) == 3:
+        if level:
+            return ["front", int(aline[1]), (float(aline[9]), float(aline[10]),
+                 float(aline[11])), int(aline[2])]
         return ["front", int(aline[1]), (float(aline[9]), float(aline[10]),
                  float(aline[11])), float(aline[12])]
 
     # hit face 4, the back detector
     if int(aline[4]) == 4:
+        if level:
+            return ["back", int(aline[1]), (float(aline[9]), float(aline[10]),
+                 float(aline[11])), int(aline[2])]
         return ["back", int(aline[1]), (float(aline[9]), float(aline[10]),
                  float(aline[11])), float(aline[12])]
 
@@ -81,7 +87,7 @@ def parse_segs_data(D, aline):
     return [int(aline[1]), (float(aline[9]), float(aline[10]),
              float(aline[11]))]
 
-def parse_segs(fid, numseg):
+def parse_segs(fid, numseg, level=False):
     """
     a utility used by parse. goes through a ray and finds relevant data
     for rays that hit detector 3 or 4
@@ -95,7 +101,7 @@ def parse_segs(fid, numseg):
     for idx in range(numseg+1):
         aline = fid.readline()
 
-        D.append(parse_segs_data(D, aline))
+        D.append(parse_segs_data(D, aline, level))
 
         if D[idx][0] == "front":
             R.append((1, D[idx][3], pathlength(D, D[idx][2], D[idx][1])))
@@ -104,7 +110,7 @@ def parse_segs(fid, numseg):
     
     return R
 
-def parse(filename, numrays):
+def parse(filename, numrays, level=False):
     """
     Reads file, returns an array of rays' (front/back, level, pathlength)
     REQUIRES: face 4 is back detector, face 3 is front detector
@@ -143,7 +149,7 @@ def parse(filename, numrays):
         # this results in [[r1],[r2],[r3]...]
         # A.append(parse_segs(fid, numseg))
         # this results in [r1,r2,r3...]
-        A += parse_segs(fid, numseg)
+        A += parse_segs(fid, numseg, level)
  
         idx += 1
     fid.close()
@@ -185,17 +191,32 @@ t = time.time()
 
 data_path = '../Crystal Data/'
 
-A = np.array(parse(data_path+'5000-4.txt',5000))
+_numrays_ = 5000
+A = np.array(parse('%d-2-2.txt' % _numrays_ ,_numrays_, level=True))
 #B = np.array(parse(data_path+'5000-3.txt',5000))
 #C = np.array(parse(data_path+'5000-2.txt',5000))
-_c_ = 299792458*1000
+_c_ = 299792458*1000 #since distance measured in millimeters
 _n_ = 1.82
 _mc_ = _n_/_c_
 
 iflog = True
 
-A2 = pl.hist((A[:,2]*_mc_), bins=50, weights=A[:,1], alpha=0.5, label="4",
+Af = onlyfront(A)
+Ab = onlyback(A)
+
+_split_=2
+_levelweight_=(1/_split_)
+#1/split ^ level
+#, weights=_levelweight_ ** (Af[:,1]-1)
+
+#, weights=Af[:,1]
+#, range=(np.amin(Af[:,2]*_mc_), 2.5e-9)
+A1 = pl.hist((Af[:,2]*_mc_), bins=50, range=(np.amin(Af[:,2]*_mc_), 2.5e-9), weights=_levelweight_ ** (Af[:,1]-1), alpha=0.5, label="front",
              log=iflog)
+
+A2 = pl.hist((Ab[:,2]*_mc_), bins=50, range=(np.amin(Ab[:,2]*_mc_), 2.5e-9), weights=_levelweight_ ** (Ab[:,1]-1), alpha=0.5, label="back",
+             log=iflog)
+
 #B2 = pl.hist(B[:,2]*_mc_, bins=50, weights=B[:,1], alpha=0.5, label="3",
 #             log=iflog)
 #C2 = pl.hist(C[:,2]*_mc_, bins=50, weights=C[:,1], alpha=0.5, label="2",
@@ -206,13 +227,17 @@ pl.legend()
 pl.xlabel("Time (seconds)")
 pl.ylabel("Total Intensity")
 
-A3 = specs(A,A2)
-#B3 = specs(B,B2)
-#C3 = specs(C,C2)
-
+'''
+bin_edges = A1[1]
+A_th1 = theoretical_bins(bin_edges,NN=_numrays_)
+pl.plot(edge2cen(bin_edges), A_th1, '.')
 bin_edges = A2[1]
-A_th = theoretical_bins(bin_edges)
-pl.plot(edge2cen(bin_edges), A_th, '.')
+A_th2 = theoretical_bins(bin_edges,NN=_numrays_)
+pl.plot(edge2cen(bin_edges), A_th2, '.')
+'''
+#sumAth = A_th1 + A_th2
+
+#pl.plot(edge2cen(bin_edges), sumAth, '.')
 if iflog:
     pl.gca().set_yscale('log')
     
